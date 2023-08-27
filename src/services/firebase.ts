c7c2c9e329260fd879e45a3081fb537d81c24b5f
KARBOU Aristide <aristidetiger12@gmail.com>
2023-08-27 02:09:50 +0000

    add of default data

--- src/services/firebase.ts
+++ src/services/firebase.ts
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import 'firebase/auth';
 import { getAuth } from "firebase/auth";
+import { getFirestore } from "firebase/firestore";
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
@@ -16,5 +17,6 @@ const firebaseConfig = {
 const app = initializeApp(firebaseConfig);
 
 export const auth = getAuth();
+export const db = getFirestore();
 
 export default app;
